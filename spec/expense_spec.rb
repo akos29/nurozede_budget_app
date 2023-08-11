c6eae4a6395e79ccac4194f4b68fffe25ebd6479
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-11 20:21:21 +0200

    Add test for expense

---
+++ spec/expense_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Expense, type: :model do
+  subject do
+    user = User.create! name: 'john', email: 'john@test.com', password: '123456'
+    Expense.create! name: 'wall street land', amount: 2_000_000, user: user
+  end
+
+  it 'test if Expense is invalid without a name' do
+    expect(subject).to be_valid
+  end
+
+  it 'test if Expense is invalid without a user' do
+    expect(subject).to be_valid
+    subject.user = nil
+    expect(subject).to be_invalid
+  end
+
+  it 'test if Expense has a valid amount' do
+    expect(subject).to be_valid
+  end
+end
