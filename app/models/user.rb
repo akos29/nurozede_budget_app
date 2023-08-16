241780f532ed901d774efc6c148c8602dc390b72
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-17 01:42:57 +0200

    Add the necessary preparation for using active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   has_many :expenses, dependent: :destroy
   has_many :groups, dependent: :destroy
+  has_one_attached :main_image
+
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless main_image.attached?
+
+    errors.add(:main_image, 'is too big') unless main_image.byte_size <= 1.megabyte
+
+    acceptable_types = ['image/jpeg', 'image/png']
+    return if acceptable_types.include?(main_image.content_type)
+
+    errors.add(:main_image, 'must be a JPEG or PNG')
+  end
 end
