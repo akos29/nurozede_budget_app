e2abdee56f920b9e0945dd85b2dcd590418d2e14
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-17 13:26:24 +0200

    Update the pending test

--- spec/views/pages/splash.html.erb_spec.rb
+++ spec/views/pages/splash.html.erb_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'pages/splash.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'displays the splash page' do
+    render
+    expect(rendered).to match(/Nurozede App/)
+  end
+
+  it 'displays title' do
+    render
+    expect(rendered).to match(/Nurozede Budget App - Welcome/)
+  end
+
+  it 'displays the sign up button' do
+    render
+    expect(rendered).to match(/Sign Up/)
+  end
+
+  it 'displays the sign in button' do
+    render
+    expect(rendered).to match(/Log In/)
+  end
 end
